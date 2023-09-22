6722a067eb59c305d6a03d3e23a62a680ceda116
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-21 22:13:54 -0700

    check

--- bcard.js
+++ bcard.js
@@ -2,18 +2,20 @@
 
 // grab the elements we are going to work with the make card interactive
 
-
-const jobTitle = document.querySelector('.jobTitle')
 const container = document.querySelector('.container')
-const contact = document.querySelector('.contact')
-const email = document.querySelector('.email')
+
+const name = document.querySelector('.name')
+const jobTitle = document.querySelector('.jobTitle')
 const skillSet = document.querySelector('.skillSet')
-const social = document.querySelector('.social')
-const backList = document.querySelector('.backList')
 const skillTitle = document.querySelector('.skillTitle')
 
+// back of card
+const contact = document.querySelector('.contact')
+const email = document.querySelector('.email')
+const social = document.querySelector('.social')
+const backList = document.querySelector('.backList')
 // button DOM
-const flipCard = document.querySelector('.flipCard')
+const flipBtn = document.querySelector('.flipCard')
 
 
 
