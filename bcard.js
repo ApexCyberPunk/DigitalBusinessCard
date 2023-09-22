5b445e4481d2eeb6e5987718991b32eb9c286005
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-21 22:15:23 -0700

    deletion

--- bcard.js
+++ bcard.js
@@ -19,34 +19,8 @@ const flipBtn = document.querySelector('.flipCard')
 
 
 
-// div class="container">
-//     <h1 class="name">Thomas</h1>
-//     <img src="./Images/happy tomatoe.png" width="200px">
-//     <!-- everyOther Element Styles the back of the card -->
-//     <h1 class="contact">Contact Info:</h1>
-//     <p class="jobTitle">Web Developer</p>
-//        <!-- hidden -->
-//     <p class="email">Email: carterRelations@gmail.com</p>
-//     <p class="skillSet">
-//         <!-- hidden for flip -->
-//         <h1 class="social">Social Media Links</h1>
-//         <ol class="backCard">
-//             <li>Github</li>
-//             <li>LinkedIn</li>
-//         </ol>
-//         <!-- end of flip -->
-//         <h1 class="skillTitle">Skills</h1>
-//         <ol>
-//             <li>JAVASCRIPT</li>
-//             <li>HTML</li>
-//             <li>REACT</li>
-//             <li>CSS</li>
-//             <li>LINUX</li>
-//         </ol>
-//     </p>
 
-// </div>
 
-// <button class="flipCard">FlipCard</button>
+
 
 
